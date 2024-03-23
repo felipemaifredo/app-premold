8f500ad0fde1a3c8bbdd76b053cb39d3f8740d6a
felipemaifredo <felipemaifredo@gmail.com>
2024-03-23 01:19:55 -0300

    att

--- src/ui/sections/ContentHero.js
+++ src/ui/sections/ContentHero.js
@@ -9,13 +9,15 @@ export const ContentHero = () => {
                 <h1>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem</h1>
                 <BtnTogether1 />
             </div>
-            <div className="content-hero-card">
-                <p className="title-content-hero-card">500+</p>
-                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem</p>
-            </div>
-            <div className="content-hero-card">
-                <p className="title-content-hero-card">500+</p>
-                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem</p>
+            <div className="container-cards">
+                <div className="content-hero-card">
+                    <p className="title-content-hero-card">500+</p>
+                    <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem</p>
+                </div>
+                <div className="content-hero-card">
+                    <p className="title-content-hero-card">500+</p>
+                    <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem</p>
+                </div>
             </div>
         </div>
     )
